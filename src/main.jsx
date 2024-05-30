bc211f067cc12355a8ffaa1f634ee1a56af47e56
J-992 <75342062+J-992@users.noreply.github.com>
2024-05-30 12:55:46 -0400

    l

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDom from 'react-dom/client';
 import App from './App';
-import 'src/index.css';
+import './index.css';
 
 ReactDom.createRoot(document.getElementById('root')).render(<App />);
 
